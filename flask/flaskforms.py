44e643f7a5d7b1863adc5422e4227bd2c7127f79
TortoisegitAnupam <analyticsprojects10@gmail.com>
2023-02-01 23:14:10 +0530

    added django project for aws

--- flask/flaskforms.py
+++ flask/flaskforms.py
@@ -2,6 +2,8 @@ from flask import Flask, request, render_template
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 
+from redis import Redis
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'mykey'
 
