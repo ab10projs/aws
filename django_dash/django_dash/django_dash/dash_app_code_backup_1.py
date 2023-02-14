3c2a8a70a14194ea931c0b7827576252dd5e557b
TortoisegitAnupam <analyticsprojects10@gmail.com>
2023-02-14 22:02:23 +0530

    3 d plots added

--- django_dash/django_dash/django_dash/dash_app_code_backup_1.py
+++ django_dash/django_dash/django_dash/dash_app_code_backup_1.py
@@ -8,23 +8,32 @@ from django_plotly_dash import DjangoDash
 
 import plotly.graph_objs as go
 import pandas as pd
-import plotly.graph_objects as go
-import numpy as np
 
 external_stylesheets=['https://codepen.io/amyoshino/pen/jzXypZ.css']
 app = DjangoDash('dash_integration_id')
 # Create a DataFrame from the .csv file:
 print(os.getcwd(), "pppppppppp")
 df = pd.read_csv('templates/OldFaithful.csv')
-pts = np.loadtxt(np.DataSource().open('https://raw.githubusercontent.com/plotly/datasets/master/mesh_dataset.txt'))
-x, y, z = pts.T
-fig = go.Figure(data=[go.Mesh3d(x=x, y=y, z=z, color='lightpink', opacity=0.50)])
 
 # Create a Dash layout that contains a Graph component:
 app.layout = html.Div([
     dcc.Graph(
         id='old_faithful',
-        figure=fig
+        figure={
+            'data': [
+                go.Scatter(
+                    x = df['X'],
+                    y = df['Y'],
+                    mode = 'markers'
+                )
+            ],
+            'layout': go.Layout(
+                title = 'Old Faithful Eruption Intervals v Durations',
+                xaxis = {'title': 'Duration of eruption (minutes)'},
+                yaxis = {'title': 'Interval to next eruption (minutes)'},
+                hovermode='closest'
+            )
+        }
     )
 ])
 
