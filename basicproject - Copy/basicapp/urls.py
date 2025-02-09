431ed157e66b199a330cba35ca64fe22801652b1
ab10projs <analyticsprojects10@gmail.com>
2025-02-09 10:03:38 +0530

    Cleanup

--- basicproject - Copy/basicapp/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import views
-from basicapp.dash_apps.finished_apps import sinpleexampe
-urlpatterns = [
-path('',views.basicapp, name= 'basicapp')
-]
--- basicproject - Copy/basicapp/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-
-
-# Create your views here.
-def basicapp(requests):
-    return render(requests, 'basicapp/welcome.html')
-
